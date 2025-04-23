4692e38fe150afcabc24fefd64a5c5d7cea69503
alxjnsk005 <alxjnsk005@gmail.com>
2025-04-23 18:38:00 -0400

    04/23/2025
    first edition

---
+++ story_content/6eMJwhuu4Et_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.59.26702.0%0D%0A%0D%0A00:00:00.001%20--%3E%2000:00:03.624%0D%0AHow%20long%20do%20trade%20secrets%20protections%20last?%0D%0A%0D%0A"}')
